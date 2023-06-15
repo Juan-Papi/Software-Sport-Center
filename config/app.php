449d6d6f84f7baed3d413011854e52aad746dfa8
Juan Pablo Rodriguez <pablojuan18sc35@gmail.com>
2023-06-14 23:09:17 -0400

    por si acaso

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
@@ -232,7 +231,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
     ],
 
 ];
